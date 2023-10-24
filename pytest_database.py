6f134c6f2b409f9e550af60f1274f2a4700d4468
tachophobicat <101125223+aidan-octane@users.noreply.github.com>
2023-10-24 18:45:06 -0400

    (2) add live-updating functionality
    added live-updating functionality to database, cleaned up code, clarified

--- pytest_database.py
+++ pytest_database.py
@@ -6,56 +6,12 @@ import os
 import csv
 import psycopg2
 
+# This code takes input serial data from our sensors, processes it, and stores it in an 
+# internal dictionary & an external local PostgreSQL database.
 
-
-# AS STANDS: currently extracts data from serial -> dictionary, then,
-# once done, adds it to database 
 # Nonfunctional until either:
     # We get the names of the sensors & I can create corresponding tables in DB
     # Functionality to create tables internally is implemented into the code
-# To get it to become live-updating:
-    # 1. Adds to dictionary, simulataneously adding it to DB
-        # - Can be done by simply moving code from below into the parse() function
-    # 2. Just adds to DB, forgoing dictionary 
-
-# Parameters of the database, local to my machine
-db_params = {
-    "host": "localhost",
-    "database": "postgres",
-    "user": "postgres",
-    "password": ""
-}
-
-# Establish a connection to the database
-conn = psycopg2.connect(**db_params)
-
-# Create a cursor object to interact with the database
-cursor = conn.cursor()
-
-## pass string in form (“LABEL|NAME|UNIT|TIMESTAMP|DATA”) and a dictionary 
-#ie, TEST|TEST SENSOR|Testies|1696177714958|59.000000
-#will fill dictionary with keys corasponding to the sensor name
-#Will fill queue in dictionary with key "sensor_name" with Data objects created from "string"
-#TODO: Ensure that everything that enters the dictionary is all lowercase as it will be stored in DB 
-def parse(string, dictionary):
-    string_split = string.split("|")
-
-    sensor_name = string_split[1]
-    unix = int(string_split[3])
-    data = float(string_split[4])
-    
-    my_data = Data(string_split[2],unix ,data)
-    
-    if(sensor_name in dictionary):
-        dictionary[sensor_name].put(my_data)
-        
-    else:
-        queue = Queue()
-        dictionary[sensor_name] = queue
-        dictionary[sensor_name].put(my_data)
-        
-   #print(my_data)
-    return sensor_name, my_data
 
 #stores value, unit, and the unix time
 class Data:
@@ -78,6 +34,64 @@ class Data:
     
     def __str__(self):
         return str(self.getTime()) + "|" + str(self.value) +"|"+ str(self.unit)
+    
+# Parameters of the database, local to my machine
+db_params = {
+    "host": "localhost",
+    "database": "postgres",
+    "user": "postgres",
+    "password": ""
+}
+
+# Establish a connection to the database
+conn = psycopg2.connect(**db_params)
+
+# Create a cursor object to interact with the database
+cursor = conn.cursor()
+
+# inputs 'data' into 'sensor_name' table in database
+def input_to_database(sensor_name, data):
+    try:
+        data_unix = data.getUnix()
+        data_value = data.getValue()
+        # Note: This assumes that the tables have been set-up, with each sensor being a table and 
+        #       columns UNIX, UNIT, VALUE
+        #           - This can be done by creating the tables from within code, though this would 
+        #             constantly overwrite previous data as we would need to delete everything every run 
+        # Data is inserted into rows of table as Unix | Value in the table corresponding to the sensor 
+        insert_query = "INSERT INTO " + str(sensor_name) + " (UNIX, VALUE) VALUES (\'" + str(data_unix) + "\', \'" + str(data_value) + "\')"
+        #print(insert_query)
+        cursor.execute(insert_query)
+        conn.commit()
+        print("Successfully input " + str(data_value) + " into table " + sensor_name + ".")
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        conn.rollback()
+
+
+# pass string in form (“LABEL|NAME|UNIT|TIMESTAMP|DATA”) and a dictionary 
+# ie, TEST|TEST SENSOR|Testies|1696177714958|59.000000
+# will fill dictionary with keys corresponding to the sensor name
+# Will fill queue in dictionary with key "sensor_name" with Data objects created from "string"
+
+def parse(string, dictionary):
+    string_split = string.split("|")
+    # Extracts necessary data from the string arg & puts to lower case into Data object
+    sensor_name = string_split[1].lower()
+    unix = int(string_split[3])
+    data = float(string_split[4])
+    
+    my_data = Data(string_split[2],unix ,data)
+    # Stores in appropriate dictionary
+    if(sensor_name in dictionary):
+        dictionary[sensor_name].put(my_data)
+    else:
+        queue = Queue()
+        dictionary[sensor_name] = queue
+        dictionary[sensor_name].put(my_data)
+    # Places parsed data into database
+    input_to_database(sensor_name, my_data)  
+    return sensor_name, my_data
         
 #designed to get data from the key at dictionary. Count is the number of Data objects dequed from the queue at key of dictionary
 #returns Data.__str__() of each Data object dequed. Goal is to send the strings from one computer to another
@@ -88,7 +102,6 @@ def getData(dictionary, key, count):
             list.append(dictionary[key].get().__str__())
         else:
             return list
-            
     return list
         
     
@@ -96,12 +109,16 @@ def getData(dictionary, key, count):
 dictionary = {}
 ##arduino = serial.Serial(port='COM3',  baudrate=9600, timeout=.1)##Probably needs to change
 running = True
-dump_everything_to_csv = True
-
+dump_everything_to_csv = False
 cnt = 0
+
+# Grabs data from sensor and parses it.
 while running:
+    # Modify this value to change frequency at which we process sensor data 
     ##time.sleep(0.05)
-    line = "TEST|test_sensor|Testies|1696177714958|59.000000"##arduino.readline()
+
+    # Set to a test string, can change to be the serial data read from sensor (which needs formatting)
+    line = "TEST|test_sensor|Testies|1696177714958|59.364234"##arduino.readline()
     ##TODO: check what arduino reads out and make it nicer to parse
     ##TODO: Set running to false somehow if you want to dump everything to a csv
     ##line = arduino.readline()
@@ -109,45 +126,15 @@ while running:
     ##if "|" in line:
     ##    line = line[2:]
     ##    line = line[:-5]
-    parse(line, dictionary)
 
-    
+    parse(line, dictionary)
+    # Arbitrary test count to simulate some data being entered to the program w/ our test string
     cnt+=1
-    print("Parse: " + str(cnt))
-    if cnt>10:
+    if cnt>5:
         running = False
 
 
-# Commits the queues of data in each sensor in dictionary to the database.
-    # Note: Can be made live-updating by simply working this into the parse() function
-try:
-    for sensor, data_queue in dictionary.items():
-    # SQL query, needs to be tested
-    # Note: This assumes that the tables have been set-up, with each sensor being a table and 
-    #       columns UNIX, UNIT, VALUE
-    #           - This can be done by creating the tables from within code, though this would 
-    #             constantly overwrite previous data as we would need to delete everything every run 
-         # Data is inserted into rows of table as Unix | Unit | Value in the table corresponding to the sensor 
-        #data_to_insert = [(data.getUnix(), data.getValue()) for data in data_queue.queue]
-
-        for data in data_queue.queue:
-         data_unix = data.getUnix()
-         data_value = data.getValue()
-         insert_query = "INSERT INTO test_sensor (UNIX, VALUE) VALUES (\'" + str(data_unix) + "\', \'" + str(data_value) + "\')"
-         print(insert_query)
-         cursor.execute(insert_query)
-         conn.commit()
-
-except Exception as e:
-    print(f"Error: {str(e)}")
-    conn.rollback()
-
-finally:
-    cursor.close()
-    conn.close()
-
-
-       
+#
 if(dump_everything_to_csv):
     #creating a folder named 'sensor_data' to store data if it does not exist
     print("wowzer")
@@ -204,10 +191,10 @@ if(dump_everything_to_csv):
                         dict = {'Time': data.getTime(), 'Data': data.getValue(), 'unit':data.getUnit()}
                         
                     writer.writerow(dict)
-                
-                
-                
-        
                 csvfile.close()
         
     write_data_to_file(dictionary, folder_path)
+
+# Close data leaks - might need to move to another part of code
+cursor.close()
+conn.close()
