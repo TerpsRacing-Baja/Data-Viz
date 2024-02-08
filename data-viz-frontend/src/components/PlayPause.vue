30612752aeddf1c22ff1099cbb9c3059f591c01a
Asher Anand <asherganand@gmail.com>
2024-02-08 13:13:37 -0500

    Got a play/pause button kicking

---
+++ data-viz-frontend/src/components/PlayPause.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
+const play = ref(false)
+</script>
+
+<template>
+    <button @click="play = !play">
+        <font-awesome-icon :icon="play ? faPlay : faPause"></font-awesome-icon>
+    </button>
+</template>
+
+<style scoped></style>
