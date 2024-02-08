79a4974d835fb1052ae97dbd571412e9d1d7d340
Asher Anand <asherganand@gmail.com>
2024-02-08 13:24:17 -0500

    Range slider for playback speed

--- data-viz-frontend/src/components/PlayPause.vue
+++ data-viz-frontend/src/components/PlayPause.vue
@@ -3,12 +3,14 @@ import { ref } from 'vue';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
 const play = ref(false)
+const speedPercent = ref(100)
 </script>
 
 <template>
     <button @click="play = !play">
         <font-awesome-icon :icon="play ? faPlay : faPause"></font-awesome-icon>
     </button>
+    <input v-model="speedPercent" type="range" min="0" max="200" class="slider" />
 </template>
 
 <style scoped></style>
