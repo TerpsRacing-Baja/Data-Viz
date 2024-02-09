8c6fcd393511b5e1fa07e0d23ffe0e060f482fda
Eric Xu <ericxu1728@gmail.com>
2024-02-09 16:19:35 -0500

    Added some code to test

--- data-viz-frontend/src/components/PlayPause.vue
+++ data-viz-frontend/src/components/PlayPause.vue
@@ -4,7 +4,7 @@
         <button @click="play = !play">
             <font-awesome-icon :icon="play ? faPause : faPlay"></font-awesome-icon>
         </button>
-        <input v-model="speedPercent" type="range" min="0" max="200" class="slider" @input="handleSpeedChange" />
+        <input v-model="speedPercent" type="range" min="0" max="200" class="slider" @change="getReponse" />
         <label>Speed: {{ speedPercent }}%</label>
     </div>
 </template>
@@ -13,15 +13,35 @@
 import { ref } from 'vue';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
-
+import axios from 'axios'
 
 const play = ref(false)
 const speedPercent = ref(100)
+
+const epic = () => {
+    console.log("epic")
+}
 
 const handleSpeedChange = (event: Event) => {
     const target = event.target as HTMLInputElement;
     speedPercent.value = parseInt(target.value);
 }
+
+const getReponse = (event: Event) => {
+    const path = 'http://localhost:5000/shark'
+    const target = event.target as HTMLInputElement;
+    speedPercent.value = parseInt(target.value);
+
+    axios.get(path)
+    .then ((res) => { 
+        console.log(res.data)
+                
+    }) 
+    .catch ((err) => {
+        console.error(err)
+    });
+}
+
 </script>
 
 <style scoped>
