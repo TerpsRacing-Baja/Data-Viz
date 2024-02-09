f9de6458ae3be74d98f78e4578984a74a7e0b197
Eric Xu <ericxu1728@gmail.com>
2024-02-08 20:07:36 -0500

    Update PlayPause.vue

--- data-viz-frontend/src/components/PlayPause.vue
+++ data-viz-frontend/src/components/PlayPause.vue
@@ -1,20 +1,27 @@
-<script setup lang="ts">
-import { ref } from 'vue';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
-const play = ref(false)
-const speedPercent = ref(100)
-</script>
-
 <template>
     <div id="playback">
         Playback Controls:
         <button @click="play = !play">
             <font-awesome-icon :icon="play ? faPause : faPlay"></font-awesome-icon>
         </button>
-        <input v-model="speedPercent" type="range" min="0" max="200" class="slider" />
+        <input v-model="speedPercent" type="range" min="0" max="200" class="slider" @input="handleSpeedChange" />
+        <label>Speed: {{ speedPercent }}%</label>
     </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faPlay, faPause } from '@fortawesome/free-solid-svg-icons'
+
+const play = ref(false)
+const speedPercent = ref(100)
+
+const handleSpeedChange = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    speedPercent.value = parseInt(target.value);
+}
+</script>
 
 <style scoped>
 #playback {
